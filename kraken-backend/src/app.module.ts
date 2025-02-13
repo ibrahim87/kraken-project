3ee33d234415aee0e37f44314b68790b022919f6
ibrahim <ibrahim.daboussi@gmail.com>
2025-02-13 18:15:52 +0100

    add OPS part

--- kraken-backend/src/app.module.ts
+++ kraken-backend/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ProductsModule } from "./products/products.module";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || "mongodb://localhost:27017/kraken",
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
