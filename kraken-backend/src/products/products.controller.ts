0eee09e19ef4127a47efe04d281795fac5b4d1d4
ibrahim <ibrahim.daboussi@gmail.com>
2025-02-13 17:25:48 +0100

    new endpoint to import products

--- kraken-backend/src/products/products.controller.ts
+++ kraken-backend/src/products/products.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Product } from "./product.schema";
+import { ProductsService } from "./products.service";
 
-@Controller('products')
-export class ProductsController {}
+@Controller("products")
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  /**
+   * import products
+   * @param products
+   */
+  @Post()
+  async create(@Body() products: Product[]) {
+    for (const product of products) {
+      await this.productsService.createOrUpdate(product);
+    }
+    return { message: "products has been imported successfully" };
+  }
+
+  /**
+   * get all products
+   */
+  @Get()
+  async findAll() {
+    return this.productsService.findAll();
+  }
+}
